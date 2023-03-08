ca1f6bd1c376a79ff2af85cddd80de4d579dbdab
maw1e <126750934+maw1e@users.noreply.github.com>
2023-03-08 11:42:53 +0800

    Add files via upload

---
+++ Cebu Blog Website/functions.js
@@ -0,0 +1,104 @@
+const hamburger = document.querySelector(".hamburger");
+const rightSection = document.querySelector(".right-section");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    rightSection.classList.toggle("active");
+})
+
+document.getElementById('button1').addEventListener('click', function(){
+    document.getElementById('bgmodal1').style.display = 'flex';
+})
+document.getElementById('close1').addEventListener('click', function(){
+    document.getElementById('bgmodal1').style.display = 'none';
+})
+
+
+document.getElementById('button2').addEventListener('click', function(){
+    document.getElementById('bgmodal2').style.display = 'flex';
+})
+document.getElementById('close2').addEventListener('click', function(){
+    document.getElementById('bgmodal2').style.display = 'none';
+})
+
+
+document.getElementById('button3').addEventListener('click', function(){
+    document.getElementById('bgmodal3').style.display = 'flex';
+})
+document.getElementById('close3').addEventListener('click', function(){
+    document.getElementById('bgmodal3').style.display = 'none';
+})
+
+
+document.getElementById('button4').addEventListener('click', function(){
+    document.getElementById('bgmodal4').style.display = 'flex';
+})
+document.getElementById('close4').addEventListener('click', function(){
+    document.getElementById('bgmodal4').style.display = 'none';
+})
+
+
+document.getElementById('button5').addEventListener('click', function(){
+    document.getElementById('bgmodal5').style.display = 'flex';
+})
+document.getElementById('close5').addEventListener('click', function(){
+    document.getElementById('bgmodal5').style.display = 'none';
+})
+
+
+document.getElementById('button6').addEventListener('click', function(){
+    document.getElementById('bgmodal6').style.display = 'flex';
+})
+document.getElementById('close6').addEventListener('click', function(){
+    document.getElementById('bgmodal6').style.display = 'none';
+})
+
+
+document.getElementById('button7').addEventListener('click', function(){
+    document.getElementById('bgmodal7').style.display = 'flex';
+})
+document.getElementById('close7').addEventListener('click', function(){
+    document.getElementById('bgmodal7').style.display = 'none';
+})
+
+
+document.getElementById('button8').addEventListener('click', function(){
+    document.getElementById('bgmodal8').style.display = 'flex';
+})
+document.getElementById('close8').addEventListener('click', function(){
+    document.getElementById('bgmodal8').style.display = 'none';
+})
+
+
+document.getElementById('button9').addEventListener('click', function(){
+    document.getElementById('bgmodal9').style.display = 'flex';
+})
+document.getElementById('close9').addEventListener('click', function(){
+    document.getElementById('bgmodal9').style.display = 'none';
+})
+
+
+document.getElementById('button10').addEventListener('click', function(){
+    document.getElementById('bgmodal10').style.display = 'flex';
+})
+document.getElementById('close10').addEventListener('click', function(){
+    document.getElementById('bgmodal10').style.display = 'none';
+})
+
+
+document.getElementById('button11').addEventListener('click', function(){
+    document.getElementById('bgmodal11').style.display = 'flex';
+})
+document.getElementById('close11').addEventListener('click', function(){
+    document.getElementById('bgmodal11').style.display = 'none';
+})
+
+
+document.getElementById('button12').addEventListener('click', function(){
+    document.getElementById('bgmodal12').style.display = 'flex';
+})
+document.getElementById('close12').addEventListener('click', function(){
+    document.getElementById('bgmodal12').style.display = 'none';
+})
+
+
